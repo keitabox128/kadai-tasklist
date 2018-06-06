4bea06d4fb4fe9cc3a2c16104404f912d118d9dd
keitabox <mixguril@gmail.com>
2018-06-06 07:17:53 +0000

    status column

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,15 +4,11 @@
 
     <h1>タスク新規作成ページ</h1>
     
-    @if (count($errors) > 0)
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif
-    
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
+    
+        {!! Form::label('status', '状態:') !!}
+        {!! Form::text('status') !!}
+    
     
         {!! Form::label('content', 'タスク:' ) !!}
         {!! Form::text('content') !!}
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,17 +4,12 @@
 
     <h1>id:{{ $task->id }} のタスク編集ページ</h1>
 
-    @if (count($errors) > 0)
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif
-
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
     
-        {!! Form::label('content', 'メッセージ:') !!}
+        {!! Form::label('status', '状態:') !!}
+        {!! Form::text('status) !!}
+       
+        {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
         
         {!! Form::submit('更新') !!}
