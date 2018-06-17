282916127c4abdcf604d0b1035083d5200f649b3
keitabox <mixguril@gmail.com>
2018-06-17 05:33:52 +0000

    modify bootstrap

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <div class="row">
-        <div class="col-xs-12 col-sm-8 col-md-8 col-lg-6">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
         <h1>タスク一覧</h1>
     
         @if (count($tasks) > 0)
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="row">
-        <div class="col-xs-12 col-sm-8 col-md-8 col-lg-6">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
         <h1>id = {{ $task->id }}のタスク一覧の詳細ページ</h1>
    
         <table class="table table-bordered">   
